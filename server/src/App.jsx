e0de275d92f43fac2f32e78be7b56a7c2e84593f
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-05-13 10:29:40 +0200

    start

---
+++ server/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Registrazione from "./registrazione";
+
+function App() {
+  return (
+    <>
+      <Registrazione></Registrazione>
+    </>
+  );
+}
+
+export default App;
