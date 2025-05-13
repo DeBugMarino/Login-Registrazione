bc916f769b61c70d43fc59307c297a966561ce51
DeBugMarino <159265842+DeBugMarino@users.noreply.github.com>
2025-05-13 12:02:16 +0200

    login

---
+++ sistemalogin/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Registrazione from "./components/registrazione";
+import Login from "./components/login";
+function App() {
+  return (
+    <>
+      <Registrazione></Registrazione>
+      <Login></Login>
+    </>
+  );
+}
+
+export default App;
