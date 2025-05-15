c10a6d713bbae5d546447d702ab80b66712df861
Danichiesa03 <edoardochiesa3@gmail.com>
2025-05-15 12:03:37 +0200

    d

--- sistemalogin/src/components/login.jsx
+++ sistemalogin/src/components/login.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function Login() {
   const [data, setData] = useState({});
-  const [message, setMessage] = useState(null);
+  const { user, login, message } = useAuth();
   const navigazione = useNavigate();
-  const { user, setUser } = useContext(AuthContext);
 
   function handleChange(event) {
     setData((prev) => ({
@@ -18,29 +16,16 @@ export default function Login() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-
     try {
-      const response = await fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(data),
-      });
-
-      const result = await response.json();
-
-      if (response.ok) {
-        setMessage(result.message);
-        setUser(result.user);
-        console.log(user);
+      const logged = await login(data);
+      console.log(user);
+      if (user) {
         setTimeout(() => {
           navigazione("/dashboard");
         }, 5000);
-      } else {
-        setMessage(result.message);
       }
     } catch (error) {
       console.error(error);
-      setMessage("errore");
     }
   }
 
@@ -62,13 +47,13 @@ export default function Login() {
         <button type="submit">login</button>
       </form>
       {message && <p>{message}</p>}
-      {user && (
+      {/* {user && (
         <div>
           <p>{user.nome}</p>
           <p>{user.email}</p>
           <p>{user.eta}</p>
         </div>
-      )}
+      )} */}
     </>
   );
 }
--- sistemalogin/src/context/AuthContext.jsx
+++ sistemalogin/src/context/AuthContext.jsx
@@ -1,14 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const AuthContext = createContext();
+const AuthContext = createContext();
+export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const [message, setMessage] = useState(null);
+
+  async function login(data) {
+    try {
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setMessage(result.message);
+        setUser(result.user);
+        console.log("if");
+        console.log(result.user);
+      } else {
+        setMessage(result.message);
+        setUser(null);
+        console.log("else");
+      }
+    } catch (error) {
+      console.log("catch");
+      console.error(error);
+      setMessage("errore");
+      setUser(null);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={(user, setUser)}>
+    <AuthContext.Provider value={{ user, login, message }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
-//ciao
