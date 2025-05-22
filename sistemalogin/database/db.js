ba9f731ae018d81e64abd0cbf96bd283fd9aef07
antonio carè <care.antonio17@gmail.com>
2025-05-22 16:04:29 +0200

    Creato primo dataBase

--- sistemalogin/database/db.js
+++ sistemalogin/database/db.js
@@ -9,4 +9,19 @@ const db = dataBase({
   password: "postgres",
 });
 
+db.none(
+  `CREATE TABLE IF NOT EXISTS utenti (
+  id SERIAL PRIMARY KEY, 
+  nome TEXT NOT NULL, 
+  cognome TEXT NOT NULL, 
+  email TEXT NOT NULL UNIQUE,
+   eta INT NOT NULL CHECK( eta >= 18), 
+   password TEXT NOT NULL 
+  )`
+)
+  .then(() => console.log("Tabella creata correttamente"))
+  .catch((error) =>
+    console.error("Errore durante la creazione della tabella", error)
+  );
+
 export default db;
